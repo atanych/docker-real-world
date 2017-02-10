f56bef90674e013de8e5506aca4a6711549be483
atanych <atanych@gmail.com>
2017-02-10 23:38:55 +0300

    manage permissions in init script

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git clone git@github.com:atanych/docker-chat.git chat
+sudo chmod 777 -R docker_config/docker_sockets
 #docker-compose run ruby bash -c 'bundle install --path vendor/bundle --binstubs'
