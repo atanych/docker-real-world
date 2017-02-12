ee23b0595aa4826aa04d0a22f0afd3dc69d66d91
atanych <atanych@gmail.com>
2017-02-12 21:55:04 +0300

    clean the code

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 git clone git@github.com:atanych/docker-chat.git chat
 sudo chmod 777 -R docker_config/docker_sockets
-#docker-compose run ruby bash -c 'bundle install --path vendor/bundle --binstubs'
